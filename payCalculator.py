9e0e2bd784d13624e39f068387b51593f2cdafe4
Ryan Read <ryan.read@veteransunited.com>
2022-01-12 20:51:40 -0600

    initial commit

---
+++ payCalculator.py
@@ -0,0 +1,9 @@
+def calculatePay():
+    # Implement your solution in between the two comments here
+    # ***important remember you must set your final answer to a variable named totalPay
+    print("calculating pay")
+    # End of assignment
+    print(totalPay)
+
+##ignore me
+calculatePay()
