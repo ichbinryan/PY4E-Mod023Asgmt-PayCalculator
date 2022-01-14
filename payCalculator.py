663206c1c1084982f30b3dbd199dbdbbcef23f04
Ryan Read <ryan.read@veteransunited.com>
2022-01-14 14:40:01 -0600

    ready for primetime

--- payCalculator.py
+++ payCalculator.py
@@ -1,9 +1,11 @@
 def calculatePay():
-    # Implement your solution in between the two comments here
-    # ***important remember you must set your final answer to a variable named totalPay
+    # Implement your solution in between the two comment blocks
     print("calculating pay")
-    # End of assignment
-    print(totalPay)
 
-##ignore me
-calculatePay()
+    # end assignment
+
+## if you want to test locally before you try to sync
+## uncomment calculatePay() and run > python payCalculator.py
+## ***IMPORTANT*** please recomment before you submit/sync your assignment.
+## OR YOUR TEST WILL NOT RUN
+# calculatePay()
--- testProgram.py
+++ testProgram.py
@@ -1,4 +1,6 @@
 import io
+from random import randint
+from secrets import randbelow
 import sys
 from unittest import mock
 from unittest.mock import Mock, patch
@@ -12,15 +14,15 @@ input_2.return_value = '27'
 input_mock = Mock()
 input_mock.side_effect = [input_1, input_2]
 
-with mock.patch('builtins.input', input_mock):
-        
-    def test_payCalculator_prints_correct_result(capfd, monkeypatch):
-        #monkeypatch.setattr('builtins.input', lambda _: "12")
-        # payCalculator.input = lambda: 'some_input'
-        # sys.stdin = io.StringIO('15\n29\n')
-        monkeypatch.setattr('sys.stdin', io.StringIO('123\n456\n'))
-        payCalculator.calculatePay()
 
-        # out, err = capfd.readouterr()
-        # print(out)
-        # assert out == '15129'
+def test_payCalculator_prints_correct_result(capfd, monkeypatch):
+    # monkeypatch.setattr('builtins.input', lambda _: "12")
+    # payCalculator.input = lambda: 'some_input'
+    # sys.stdin = io.StringIO('15\n29\n')
+    inp = randint(1, 100)
+    monkeypatch.setattr('builtins.input', lambda _:inp)
+    payCalculator.calculatePay()
+
+    out, err = capfd.readouterr()
+    print(out)
+    assert out == 'calculating pay\n{}\n'.format(inp**2)
