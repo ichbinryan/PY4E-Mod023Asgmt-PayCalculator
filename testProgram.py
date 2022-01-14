174d4961d5e4502a03ae0465ff6d3c895e990f8c
Ryan Read <ryan.read@veteransunited.com>
2022-01-14 16:03:20 -0600

    randomize test case further

--- testProgram.py
+++ testProgram.py
@@ -1,22 +1,16 @@
 import io
 from random import randint
+import random
 from unittest.mock import Mock
 import payCalculator
 
-input_1 = Mock()
-input_1.return_value = '15'
-input_2 = Mock()
-input_2.return_value = '27'
-
-input_mock = Mock()
-input_mock.side_effect = [input_1, input_2]
-
-
 def test_payCalculator_prints_correct_result(capfd, monkeypatch):
-    inp = randint(1, 100)
-    monkeypatch.setattr('builtins.input', lambda _:inp)
+    rate = randint(1, 100)
+    hours = randint(1, 100)
+    inp = [rate, hours]
+    monkeypatch.setattr('builtins.input', lambda _:inp.pop())
     payCalculator.calculatePay()
 
     out, err = capfd.readouterr()
     print(out)
-    assert out == 'calculating pay\n{}\n'.format(inp**2)
+    assert out == 'calculating pay\n{}\n'.format(rate * hours)
