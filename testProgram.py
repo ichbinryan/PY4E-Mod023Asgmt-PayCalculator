8f4ae22860918d4f97f8129df7c7c110f2cc8033
Ryan Read <ryan.read@veteransunited.com>
2022-01-14 14:06:21 -0600

    examples of different test attempts

--- testProgram.py
+++ testProgram.py
@@ -1,11 +1,26 @@
+import io
+import sys
+from unittest import mock
 from unittest.mock import Mock, patch
 import payCalculator
 
+input_1 = Mock()
+input_1.return_value = '15'
+input_2 = Mock()
+input_2.return_value = '27'
 
-def test_payCalculator_prints_correct_result(capfd):
-    payCalculator.input = lambda: '123'
-    payCalculator.calculatePay()
+input_mock = Mock()
+input_mock.side_effect = [input_1, input_2]
 
-    # //out, err = capfd.readouterr()
-    # //print(out)
-    # assert out == '15129'
+with mock.patch('builtins.input', input_mock):
+        
+    def test_payCalculator_prints_correct_result(capfd, monkeypatch):
+        #monkeypatch.setattr('builtins.input', lambda _: "12")
+        # payCalculator.input = lambda: 'some_input'
+        # sys.stdin = io.StringIO('15\n29\n')
+        monkeypatch.setattr('sys.stdin', io.StringIO('123\n456\n'))
+        payCalculator.calculatePay()
+
+        # out, err = capfd.readouterr()
+        # print(out)
+        # assert out == '15129'
