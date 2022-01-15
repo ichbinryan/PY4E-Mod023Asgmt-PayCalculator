8afb90d9c3633671ff0bd00997ede24977c993c0
Ryan Read <ryan.read@veteransunited.com>
2022-01-14 18:35:44 -0600

    remove answers you dumb dumb

--- payCalculator.py
+++ payCalculator.py
@@ -3,7 +3,7 @@ def calculatePay():
     print("calculating pay")
     # This first line is provided for you
     hrs = input("Enter Hours:")
-
+    
     # end assignment
 
 ## if you want to test locally before you try to sync
