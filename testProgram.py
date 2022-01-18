65fde01f747c69e8e998d1831287cc53f66204df
Ryan Read <ryan.read@veteransunited.com>
2022-01-17 18:24:47 -0600

    allow final answer to be a float

--- testProgram.py
+++ testProgram.py
@@ -5,12 +5,12 @@ from unittest.mock import Mock
 import payCalculator
 
 def test_payCalculator_prints_correct_result(capfd, monkeypatch):
-    rate = randint(1, 100)
+    rate = float(randint(1, 100))
     hours = randint(1, 100)
     inp = [rate, hours]
     monkeypatch.setattr('builtins.input', lambda _:inp.pop())
     payCalculator.calculatePay()
 
     out, err = capfd.readouterr()
-    print(out)
-    assert out == 'calculating pay\n{}\n'.format(rate * hours)
+    expected = 'calculating pay\n{}\n'.format(rate * hours)
+    assert out == expected
