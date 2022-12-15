534a03464a1cff7c2730a0a66e9ddcbe328660d9
Nathan Gearke <69879313+GearsandKeys@users.noreply.github.com>
2022-12-15 17:38:48 -0600

    Update payCalculator.py
    Updated to have if __name__ == "__main__":

--- payCalculator.py
+++ payCalculator.py
@@ -3,11 +3,13 @@ def calculatePay():
     print("calculating pay")
     # This first line is provided for you
     hrs = input("Enter Hours:")
-    
+
     # end assignment
 
-## if you want to test locally before you try to sync
-## uncomment calculatePay() and run > python payCalculator.py
-## ***IMPORTANT*** please recomment before you submit/sync your assignment.
-## OR YOUR TEST WILL NOT RUN
-# calculatePay()
+
+## If you want to test locally before you try to sync
+## Open your terminal and run > python payCalculator.py
+
+#Ignore this for now. 
+if __name__ == "__main__":
+    calculatePay()
